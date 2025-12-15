cac81af1ed4ff01e6d82d2c697a5554e2c410d9f
mustafa kulavuz <mh2504383@gmail.com>
2025-12-15 20:39:52 +0300

    adsense kodu eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr" suppressHydrationWarning>
+      <head>
+        {/* 🌐 Google AdSense Doğrulama Kodu */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5619569366075074"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${inter.className} bg-slate-50 text-slate-900 antialiased transition-colors duration-300 dark:bg-slate-950 dark:text-slate-100`}
       >
